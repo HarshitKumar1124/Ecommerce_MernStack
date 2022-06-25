c23f81b27c96c2a4fa2d44df6e4f507b0c55038b
HarshitKumar1124 <harshitkr1124@gmail.com>
2022-06-25 11:33:43 +0530

    Part- FrontEnd Development | Header And Footer Desihning Done ! | Routing of Home, Product , Contact and About is needed to be done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,36 @@
-import Header from "./components/Header.js"
+import Header from "./components/Header/Header.js"
 import './App.css';
-import {BrowserRouter as Router} from "react-router-dom"
-
+import {BrowserRouter as Router,Route} from "react-router-dom"
+import React,{useEffect} from "react";
+import Webfont from "webfontloader"
+import Footer from "./components/Footer/Footer.js"
+import Home from "./components/Home/Home.js"
+import Product from "./components/Product/Product.js"
+import Contact from "./components/Contact/Contact.js"
+import About from "./components/About/About.js"
 
 
 function App() {
+
+
+  useEffect(()=>{
+    Webfont.load({
+       google:{
+        families:["Quantico","Yellowtail","Roboto","Droid Sans","Chilanka"]
+       }
+    })
+  },[]);
+
+
   return (
     <Router>
       <Header/>
+      {/* <Route extact  element={<Home/>}/> */}
+      {/* <Route exact path="/product" component={Product}/>
+      <Route exact path="/contact" component={Contact}/>
+      <Route exact path="/about" component={About}/> */}
+
+      <Footer/>
     </Router>
   );
 }
